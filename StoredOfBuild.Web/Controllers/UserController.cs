575713030c2e35469b766348143c0db6ee1ecaad
tigelah <lokoportitimao2009@hotmail.com>
2018-04-27 12:57:31 -0300

    Finalizado

---
+++ StoredOfBuild.Web/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StoredOfBuild.Domain.Account;
+using StoredOfBuild.Web.Models;
+
+namespace StoredOfBuild.Web.Controllers
+{
+    [Authorize(Roles = "Admin, Manager")]
+    public class UserController : Controller
+    {
+        private readonly IManager _manager;
+
+        public UserController(IManager manager)
+        {
+
+            _manager = manager;
+        }
+
+        public IActionResult Index()
+        {
+            var users = _manager.ListAll();
+            var usersViewModel = users.Select(u => new UserViewModel { Id = u.Id, Email = u.Email });
+            return View(usersViewModel);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(UserViewModel viewModel)
+        {
+            await _manager.CreateAsync(viewModel.Email, viewModel.Password, viewModel.Role);
+            return Ok();
+        }
+    }
+}
